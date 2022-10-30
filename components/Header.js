be4ff0574354a70d34973df84d848ce275f43860
hansol <vawav11@gmail.com>
2022-10-30 16:29:18 +0900

    feat: header 및 search bar 추가

--- components/Header.js
+++ components/Header.js
@@ -1,31 +1,40 @@
 import React from "react";
 import Image from "next/image";
+import { SearchIcon } from "@heroicons/react/outline";
 
 const Header = () => {
   return (
-    <div>
+    <div className="flex items-center justify-between max-w-6xl">
       {/* Left */}
-      <div className="flex items-center justify-between max-w-6xl">
-        <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
-          <Image
-            src="https://www.asiapacdigital.com/Kr/img/ap/services/agency/AsiaPac_Instagram_logo.png"
-            layout="fill"
-            className="object-contain"
-          />
-        </div>
-        <div className="cursor-pointer h-24 w-10 relative lg:hidden">
-          <Image
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/800px-Instagram-Icon.png"
-            layout="fill"
-            className="object-contain"
-          />
-        </div>
-
-        <h1>Right sides</h1>
+      <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+        <Image
+          src="https://www.asiapacdigital.com/Kr/img/ap/services/agency/AsiaPac_Instagram_logo.png"
+          layout="fill"
+          className="object-contain"
+        />
+      </div>
+      <div className="cursor-pointer h-24 w-10 relative lg:hidden">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/800px-Instagram-Icon.png"
+          layout="fill"
+          className="object-contain"
+        />
       </div>
       {/* Middle */}
 
+      <div className="relative mt-1">
+        <div className="absolute top-2 left-2">
+          <SearchIcon className="h-5 text-gray-500" />
+        </div>
+        <input
+          type="text"
+          placeholder="Search"
+          className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+        />
+      </div>
+
       {/* Right */}
+      <h1>Right sides</h1>
     </div>
   );
 };
