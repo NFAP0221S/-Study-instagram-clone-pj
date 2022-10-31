f680429de163053135a3e5b1e83a7862e1a08b73
hansol <vawav11@gmail.com>
2022-10-31 20:06:10 +0900

    feat: header style 작업 및 icon 추가

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Image from "next/image";
-import { SearchIcon } from "@heroicons/react/outline";
+import { SearchIcon, PlusCircleIcon } from "@heroicons/react/outline";
+import { HomeIcon } from "@heroicons/react/solid";
 
 const Header = () => {
   return (
-    <div className="flex items-center justify-between max-w-6xl">
+    <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
       {/* Left */}
       <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
         <Image
@@ -34,7 +35,16 @@ const Header = () => {
       </div>
 
       {/* Right */}
-      <h1>Right sides</h1>
+
+      <div className="flex space-x-4 items-center">
+        <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-tranform duration-200 ease-out" />
+        <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-tranform duration-200 ease-out" />
+        <img
+          className="h-10 rounded-full cursor-pointer"
+          src="https://avatars.githubusercontent.com/u/102638663?v=4"
+          alt="user-image"
+        />
+      </div>
     </div>
   );
 };
